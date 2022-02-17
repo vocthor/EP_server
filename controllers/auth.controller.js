fc2abd7a3c179b282f881a6554fb4fd752b06917
Hugo LAMOUREUX <hugo.lamoureux18@gmail.com>
2022-02-18 00:53:29 +0100

    Structuration finie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,10 @@
+const config = require("../config/db");
+const mysql = require("mysql");
+const { sign } = require("jsonwebtoken");
+
+/* Crée la connexion à la BD */
+const db = mysql.createConnection(config.databaseOptions);
+
 /* Fonction qui permets d'insérer un utilisateur dans la BD à partir de données reçues du front,
     appelée avec /create    */
 module.exports.createUser = (req, res) => {
@@ -21,4 +28,34 @@ module.exports.createUser = (req, res) => {
       }
     );
   }
+};
+
+/* Fonction qui permets de vérifier si l'id reçu du front est bien présent dans la BD et si oui
+retourne un token qui va ensuite sur le front être stocké dans le session storage pour permettre
+l'authentification. Si l'id n'est pas présent retourne un objet avec un message d'erreur.
+appelée avec /getUser   */
+module.exports.getUser = (req, res) => {
+  const id = req.body.id;
+  db.query("SELECT * FROM users WHERE id= ?", [id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length > 0) {
+        const accessToken = sign(
+          {
+            prenom: result[0].prenom,
+            nom: result[0].nom,
+            pseudo: result[0].pseudo,
+            id: result[0].id,
+          },
+          "secret message"
+        );
+        res.send({
+          accessToken: accessToken,
+        });
+      } else {
+        res.send({ error: "Id no present in the db" });
+      }
+    }
+  });
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.post("/create", authController.createUser);
+router.post("/getUser", authController.getUser);
+
+/* Fonction qui permets de recup les données associées à un token d'user
+appelée avec /getUserData     */
+router.post("/getUserData", validateToken, (req, res) => {
+  res.send(req.body);
+});
 
 module.exports = router;
