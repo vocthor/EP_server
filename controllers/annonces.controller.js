145de538cb1475647b759d5ce010fdbdfec69b64
vocthor <pieceo108@gmail.com>
2022-04-12 12:09:33 +0200

    pre-admin

--- controllers/annonces.controller.js
+++ controllers/annonces.controller.js
@@ -5,23 +5,34 @@ const { sign } = require("jsonwebtoken");
 /* Crée la connexion à la BD */
 const db = mysql.createConnection(config.databaseOptions);
 
+/**
+ * Ajouter une Annonce dans la table annonce
+ * @param {*} req
+ * @param {*} res
+ */
 module.exports.addAnnonce = (req, res) => {
   const id_annonce = req.body.id_annonce;
   const id_owner = req.body.id_owner;
   const text = req.body.text;
   const contact = req.body.contact;
+  const anon = req.body.anon;
+  const date = new Date().toISOString().slice(0, 10);
 
   if (id_annonce == "" || id_owner == "" || text == "") {
     res.send("Invalid fields");
   } else {
     db.query(
-      "INSERT INTO annonces (id_annonce, id_owner, annonce, contact) VALUES (?,?,?,?)",
-      [id_annonce, id_owner, text, contact],
+      "INSERT INTO annonces (id_annonce, id_owner, annonce, contact, anon, date) VALUES (?,?,?,?,?,?)",
+      [id_annonce, id_owner, text, contact, anon, date],
       (err, result) => {
         if (err) {
           console.log(err);
           res.send("Error during insertion");
         } else {
+          console.log(
+            "Annonce ajoutée : " +
+              [id_annonce, id_owner, text, contact, anon, date]
+          );
           res.send("Values inserted");
         }
       }
@@ -29,9 +40,14 @@ module.exports.addAnnonce = (req, res) => {
   }
 };
 
-module.exports.updateAnnonces = (req, res) => {
+/**
+ * Renvoyer l'ensemble de la table annonce au front end pour les afficher
+ * @param {*} req
+ * @param {*} res
+ */
+module.exports.retrieveAnnonces = (req, res) => {
   db.query(
-    "SELECT id_annonce, id_owner, annonce, contact, nom, prenom \
+    "SELECT id_annonce, id_owner, text_annonce, contact_annonce, nom, prenom, anon \
    FROM annonces, users \
    WHERE users.id=annonces.id_owner",
     [],
@@ -42,13 +58,20 @@ module.exports.updateAnnonces = (req, res) => {
   );
 };
 
+/**
+ * Supprimer une annonce si l'on en est le propriétaire
+ * @param {*} req
+ * @param {*} res
+ */
 module.exports.deleteAnnonce = (req, res) => {
   const id_annonce = req.body.id_annonce;
   const id_owner = req.body.id_owner;
-
+  // On vérifie aussi côté backEnd
   db.query(
     "DELETE FROM annonces \
-    WHERE id_annonce= ? AND id_owner = ?",
+    WHERE id_annonce= ? AND (id_owner = ? OR id_owner IN \
+      (SELECT id FROM users \
+      WHERE role='admin' OR role='moderator'))",
     [id_annonce, id_owner],
     (err, result) => {
       if (err) {
--- routes/annonces.routes.js
+++ routes/annonces.routes.js
@@ -3,7 +3,7 @@ const annoncesController = require("../controllers/annonces.controller");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.post("/addAnnonce", annoncesController.addAnnonce);
-router.post("/updateAnnonces", annoncesController.updateAnnonces);
+router.post("/retrieveAnnonces", annoncesController.retrieveAnnonces);
 router.post("/deleteAnnonce", annoncesController.deleteAnnonce);
 
 module.exports = router;
